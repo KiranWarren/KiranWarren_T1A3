d70c5707875938e7ac24b270ae48de89f32afd5c
Kiran Warren <kiran.m.warren@gmail.com>
2023-06-27 21:15:01 +1000

    Replace function created

---
+++ src/replace_function.py
@@ -0,0 +1,10 @@
+from import_file import *
+from clear_terminal import *
+
+clear_terminal()
+print("####################################")
+print("            REPLACE WORD            ")
+print("####################################")
+import_file()
+
+
