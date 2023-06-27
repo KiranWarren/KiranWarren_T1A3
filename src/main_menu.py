e2cd1b80366be8947ecea8735661467c39356afe
Kiran Warren <kiran.m.warren@gmail.com>
2023-06-27 21:13:53 +1000

    Main menu display function created

---
+++ src/main_menu.py
@@ -0,0 +1,19 @@
+from clear_terminal import *
+
+def main_menu():
+    clear_terminal()
+    print("####################################")
+    print(" TEXT FILE MANIPULATION APPLICATION ")
+    print("####################################")
+    print("Kiran Warren 2023")
+    print('')
+    print('Functions:')
+    print('[1] Replace Word in Text File')
+    print('[2] Double Space Text File')
+    print('[3] Encrypt Text File')
+    print('[4] Decrypt Text File')
+    print('[5] Word/Character Count of Text File')
+    print('[6] Top Occurring Words in Text File')
+    print('[7] Occurrence of Word in Text File')
+    print('[Enter anything else to exit application]')
+    print('')
