30c782284f06a2b58491918c9ee7a3f543c7420f
Kiran Warren <kiran.m.warren@gmail.com>
2023-06-28 20:45:37 +1000

    Export and string/list conversion separated

--- src/export_file.py
+++ src/export_file.py
@@ -1,23 +1,28 @@
 # Export File Function
 #
-# The list passed to this function will be turned back into a string
-# The string will be written to the target text file
+# The string passed to this function will be written to the target text file.
 # The target text file will be created if it does not already exist, otherwise it will overwrite.
 
 import os
 
-def export_file(contents_list, new_file_name):
-
-    # Convert list back into a string
-    file_contents = ""
-    for i in range(0, len(contents_list)):
-        file_contents += contents_list[i]
-        if not contents_list[i] == "\n":
-            file_contents += " "
+def export_file(output_string, new_file_name):
 
     # Determine the file path.
     outputs_file_path = os.path.join(os.path.join(os.path.dirname(__file__), "outputs"), new_file_name)
 
     # Write contents to file.
     with open(outputs_file_path, 'w') as file:
-        file.write(file_contents)
+        file.write(output_string)
+
+def convert_list_to_string(substring_list, delimiter = " "):
+    output_string = ""
+
+    # Convert passed list back into a string
+    for i in range(0, len(substring_list)):
+        output_string += substring_list[i]
+        try:
+            if substring_list[i] != "\n" and substring_list[i + 1] != "\n":
+                output_string += delimiter
+        except:
+            pass
+    return output_string
