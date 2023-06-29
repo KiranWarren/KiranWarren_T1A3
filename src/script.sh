086932ec9c8783a0b759187ba454803ad3019b8a
Kiran Warren <kiran.m.warren@gmail.com>
2023-06-29 23:33:55 +1000

    Added commands to shell script

--- src/script.sh
+++ src/script.sh
@@ -1,1 +1,19 @@
-python3 src/main.py
+#!/bin/bash
+
+# Check if Python is installed
+
+# Check if pip is installed
+
+# Create virtual environment and set as current environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install package requirements
+pip install -r requirements.txt
+
+# Run 
+python3 main.py
+
+# Deactivate and remove virtual environment
+deactivate
+rm -rf venv
