021726b4a259456a53fe1a8d2f26d359339367fe
Kiran Warren <kiran.m.warren@gmail.com>
2023-07-03 20:12:56 +1000

    Created new scripts, can run without chmod

---
+++ src/text_app.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if Python is installed
+if ! [[ "$(python3 --version)" =~ "Python 3" ]]
+    then
+        echo "Sorry, python3 command is not working. Please ensure you have Python 3 installed before using this application."
+        echo "Applicated closed."
+        exit 0
+fi
+
+# Check if pip is installed
+if ! [[ "$(pip3 --version)" =~ "pip " ]]
+    then
+        echo "Sorry, pip3 command is not working. Please ensure you have PIP 3 installed before using this application."
+        echo "Applicated closed."
+        exit 0
+fi
+
+# Create virtual environment and set as current environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install package requirements into venv
+pip3 install cryptography
+
+# Run application
+python3 main.py
+
+# Deactivate and remove virtual environment upon exiting application
+deactivate
+rm -rf venv __pycache__
